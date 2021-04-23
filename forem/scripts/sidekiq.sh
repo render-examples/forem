8e08e516b455c728f1798a9b7b8367e44afd3320
scottnuma <scott.numamoto@gmail.com>
2021-04-23 10:48:58 -0700

    Sidekiq should wait for Rails

--- forem/scripts/sidekiq.sh
+++ forem/scripts/sidekiq.sh
@@ -2,4 +2,10 @@
 set -e
 
 source scripts/services.env
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' https://$APP_DOMAIN)" != "200" ]]
+do
+	echo "waiting for Rails application"
+	sleep 10
+done
 bundle exec sidekiq -c 1
