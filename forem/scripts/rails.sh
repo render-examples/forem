0af253c989b445e71bf24a60c3c4d30f1f08a888
scottnuma <scott.numamoto@gmail.com>
2021-04-23 12:48:55 -0700

    Retry app intialization

--- forem/scripts/rails.sh
+++ forem/scripts/rails.sh
@@ -3,8 +3,14 @@ set -e
 
 source scripts/services.env
 
-echo "Initializing the application"
-bin/rails app_initializer:setup
+echo "Running app_initializer:setup"
+until bin/rails app_initializer:setup
+do
+	echo "waiting before retrying app intialization"
+	sleep 60
+done
+
+echo "Running forem:setup"
 bin/rails forem:setup
 
 echo "Starting the application server"
